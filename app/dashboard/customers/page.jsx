350cef4fd5c6f49135cd3d364b67b0afa843c7c2
Mohamed Abdinor <mohamedabdinor@Mohameds-MacBook-Air.local>
2023-12-03 15:02:10 +0300

    Your commit message

---
+++ app/dashboard/customers/page.jsx
@@ -0,0 +1,10 @@
+
+import {lusitana, inter } from '@/app/ui/fonts';
+
+
+export default function Page() {
+    return <h6 className={`${inter.className} mb-8 text-xl md:text-2xl`}>
+    Customers
+  </h6>
+       
+  }
